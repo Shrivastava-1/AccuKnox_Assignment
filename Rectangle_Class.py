98d0cdda08a6dab52fe4ac6c5b1661098adb13e1
Chitransh Shrivastava <cshrivastava003@gmail.com>
2025-08-02 23:05:20 +0530

    Assignment complete

--- Rectangle_Class.py
+++ Rectangle_Class.py
@@ -4,13 +4,10 @@ class  Rectangle:
         self.width = width
     
     def __iter__(self):
-        #The First value to iterate is the length, followed by the width
         yield {'length' : self.length}
         yield {'width' : self.width}
 
-# Example usage:
-rectanglee = Rectangle(5,3)
+cust_rent = Rectangle(5,3)
 
-# Iterating over the instance
-for attribute in rectanglee:
+for attribute in cust_rent:
     print(attribute)
